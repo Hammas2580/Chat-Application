ce45087cc1380a4b274530f042d7ef02e184aa1f
HashamSalauddin2006 <your_email@example.com>
2025-05-09 20:09:19 +0500

    form Functionalitty done

--- src/components/Form/form.jsx
+++ src/components/Form/form.jsx
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { LockOutlined, MailOutlined, UserOutlined } from '@ant-design/icons';
 import { Button, Form, Input } from 'antd';
 
 const FormComponents = () => {
+    const [formState, setFormState] = useState("Sign Up");
+
+    const handleForm = () => {
+
+        formState == "Sign Up" ? setFormState("Sign In") : setFormState("Sign Up")
+
+    }
     const onFinish = (values) => {
         console.log('Received values of form: ', values);
     };
@@ -18,28 +25,32 @@ const FormComponents = () => {
                     onFinish={onFinish}
                     layout="vertical"
                 >
-                    <Form.Item
-                        name="username1"
-                        label={<span className="text-sm font-medium text-gray-700">First Name</span>}
-                        rules={[{ required: true, message: 'Please input your Username!' }]}
-                    >
-                        <Input
-                            prefix={<UserOutlined className="site-form-item-icon text-gray-400" />}
-                            placeholder="Enter your  First name"
-                            className="py-2"
-                        />
-                    </Form.Item>
-                    <Form.Item
-                        name="username2"
-                        label={<span className="text-sm font-medium text-gray-700">Last Name</span>}
-                        rules={[{ required: true, message: 'Please input your Username!' }]}
-                    >
-                        <Input
-                            prefix={<UserOutlined className="site-form-item-icon text-gray-400" />}
-                            placeholder="Enter your Second name"
-                            className="py-2"
-                        />
-                    </Form.Item>
+                    {formState == "Sign Up" &&
+                        <Form.Item
+                            name="username1"
+                            label={<span className="text-sm font-medium text-gray-700">First Name</span>}
+                            rules={[{ required: true, message: 'Please input your First Name!' }]}
+                        >
+                            <Input
+                                prefix={<UserOutlined className="site-form-item-icon text-gray-400" />}
+                                placeholder="Enter your  First name"
+                                className="py-1"
+                            />
+                        </Form.Item>
+                    }
+                    {formState == "Sign Up" &&
+                        <Form.Item
+                            name="username2"
+                            label={<span className="text-sm font-medium text-gray-700">Last Name</span>}
+                            rules={[{ required: true, message: 'Please input your Last Name!' }]}
+                        >
+                            <Input
+                                prefix={<UserOutlined className="site-form-item-icon text-gray-400" />}
+                                placeholder="Enter your Second name"
+                                className="py-2"
+                            />
+                        </Form.Item>
+                    }
                     <Form.Item
                         name="email"
                         label={<span className="text-sm font-medium text-gray-700">Email</span>}
@@ -63,40 +74,43 @@ const FormComponents = () => {
                             className="py-2"
                         />
                     </Form.Item>
-
-                    <Form.Item
-                        name="confirm"
-                        label="Confirm Password"
-                        dependencies={['password']}
-                        hasFeedback
-                        rules={[
-                            {
-                                required: true,
-                                message: 'Please confirm your password!',
-                            },
-                            ({ getFieldValue }) => ({
-                                validator(_, value) {
-                                    if (!value || getFieldValue('password') === value) {
-                                        return Promise.resolve();
-                                    }
-                                    return Promise.reject(new Error('The new password that you entered do not match!'));
+                    {
+                        formState == "Sign Up" &&
+                        <Form.Item
+                            name="confirm"
+                            label="Confirm Password"
+                            dependencies={['password']}
+                            hasFeedback
+                            rules={[
+                                {
+                                    required: true,
+                                    message: 'Please confirm your password!',
                                 },
-                            }),
-                        ]}
-                    >
-                        <Input.Password prefix={<LockOutlined/>} placeholder='Please Enter Your Confirm Password' />
-                    </Form.Item>
-
-
-
+                                ({ getFieldValue }) => ({
+                                    validator(_, value) {
+                                        if (!value || getFieldValue('password') === value) {
+                                            return Promise.resolve();
+                                        }
+                                        return Promise.reject(new Error('The new password that you entered do not match!'));
+                                    },
+                                }),
+                            ]}
+                        >
+                            <Input.Password prefix={<LockOutlined />} placeholder='Please Enter Your Confirm Password' />
+                        </Form.Item>
+                    }
                     <Form.Item className="mb-3">
                         <Button block type="primary" htmlType="submit" className="bg-blue-600 hover:bg-blue-700">
-                            Log in
+                            Sign Up
                         </Button>
                     </Form.Item>
 
                     <div className="text-center text-sm text-gray-600">
-                        Don't have an account? <a href="#" className="text-blue-600 hover:underline">Register now!</a>
+                        Don't have an account? <span onClick={handleForm} className="text-blue-600 hover:underline cursor-pointer">
+                            {
+                                formState == "Sign Up" ? "Sign In" : "Sign Up"
+                            }
+                        </span>
                     </div>
                 </Form>
             </div>
